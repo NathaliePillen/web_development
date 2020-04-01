fcca959d9b553f75677f4b91150828590bb1ac8a
Nathalie Pillen <56022155+NathaliePillen@users.noreply.github.com>
2020-04-01 11:05:03 +0200

    finishing game

--- Simon Game Challenge/game.js
+++ Simon Game Challenge/game.js
@@ -4,23 +4,79 @@ var gamePattern = [];
 
 var userClickedPattern = [];
 
+//You'll need a way to keep track of whether if the game has started or not, so you only call nextSequence() on the first keypress.
+var started = false;
+
+//2. Create a new variable called level and start at level 0.
+var level = 0;
+
+//1. Use jQuery to detect when a keyboard key has been pressed, when that happens for the first time, call nextSequence().
+$(document).keydown(function() {
+  if (!started) {
+
+    //3. The h1 title starts out saying "Press A Key to Start", when the game has started, change this to say "Level 0".
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
 $(".btn").click(function () {
     var userChosenColor = $(this).attr("id");
     userClickedPattern.push(userChosenColor);
-    console.log(userClickedPattern);
+    //console.log(userClickedPattern);
 
     playSound(userChosenColor);
+
+    animatePress(userChosenColor);
+
+    //2. Call checkAnswer() after a user has clicked and chosen their answer, passing in the index of the last answer in the user's sequence.
+    checkAnswer(userClickedPattern.length-1);
 });
 
+//1. Create a new function called checkAnswer(), it should take one input with the name currentLevel
+function checkAnswer(currentLevel) {
+
+    //3. Write an if statement inside checkAnswer() to check if the most recent user answer is the same as the game pattern. If so then log "success", otherwise log "wrong".
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+      console.log("success");
+
+      //4. If the user got the most recent answer right in step 3, then check that they have finished their sequence with another if statement.
+      if (userClickedPattern.length === gamePattern.length){
+
+        //5. Call nextSequence() after a 1000 millisecond delay.
+        setTimeout(function () {
+          nextSequence();
+        }, 1000);
+
+      }
+
+    } else {
+        var sound = new Audio("sounds/wrong.mp3");
+        sound.play(); 
+        $("body").addClass("game-over");
+        console.log("wrong");
+        startOver();
+    }
+    
+}
+
 function nextSequence(){
+
+    //6. Once nextSequence() is triggered, reset the userClickedPattern to an empty array ready for the next level.
+    userClickedPattern = [];
+
+    level++;
+    $("#level-title").text("Level " + level);
     var randomNumber = Math.floor(Math.random()*4);
-    console.log (randomNumber);
+    //console.log (randomNumber);
 
     var randomChosenColor = buttonColors[randomNumber];
-    console.log (randomChosenColor);
+    //console.log (randomChosenColor);
 
     gamePattern.push(randomChosenColor);
-    console.log(gamePattern);
+    //console.log(gamePattern);
 
     $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
 
@@ -33,3 +89,23 @@ function playSound(name){
     sound.play();
 }
 
+function animatePress(currentColor){
+    var activeColor = $("#" + currentColor);
+
+    activeColor.addClass("pressed");
+
+    setTimeout(function(){
+        $(activeColor).removeClass("pressed");        
+    },100);
+}
+
+function startOver(){
+  started = false;
+  setTimeout(function(){
+      $("body").removeClass("game-over");}
+      ,500);
+  $("#level-title").text("Press A Key to Start");
+  level=0;
+  gamePattern = [];
+  userClickedPattern=[];
+}
